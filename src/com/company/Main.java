d55d0f627a243c151d4b0949a81563f8cc1fb7aa
alt-nikitha <nikithasrikanth1711@gmail.com>
2020-12-08 15:58:29 +0530

    removed hard-coded number of nodes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 public class Main{
     public static void main(String[] args){
         MWIS x= new MWIS();
-        double[][] messagesOneCurr = new double[5][5];
-        double[][] messagesZeroCurr = new double[5][5];
+        int n=5;
+        double[][] messagesOneCurr = new double[n][n];
+        double[][] messagesZeroCurr = new double[n][n];
         double[] trackscores = {3.4, 9.1, 7.5, 4.8, 10.1};
-        int[] independentset = new int[5];
+        int[] independentset = new int[n];
         int[] independentsetprev;
         independentsetprev= new int[]{0, 0, 0, 0, 0};
-        x.initialise(messagesZeroCurr,messagesOneCurr,trackscores,independentset);
+        x.initialise(n,messagesZeroCurr,messagesOneCurr,trackscores,independentset);
 
         int [][] adj= {
                 {0, 1, 1, 0, 0},
@@ -25,10 +26,10 @@ public class Main{
         //int k=0;
         for(;;) {
 
-            messagesCurr = x.getAllMessages(messagesCurr, adj, trackscores); //getting all messages for current iteration
+            messagesCurr = x.getAllMessages(n,messagesCurr, adj, trackscores); //getting all messages for current iteration
 
             //belief propagation
-            independentset = x.allBeliefs(messagesCurr, independentset, trackscores, adj);
+            independentset = x.allBeliefs(n, messagesCurr, independentset, trackscores, adj);
 
             //check convergence
             //if(x.checkconvergence(independentsetprev,independentset)==1) break;
